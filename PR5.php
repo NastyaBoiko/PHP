5cbaec3dabbe06b6985aa8e0c434a5ea2a705ed2
Nastya Boiko <killer99whale@mail.ru>
2023-03-21 19:51:00 +0300

    2 task

--- PR5.php
+++ PR5.php
@@ -40,91 +40,132 @@ class ExceptionMinus extends Exception
     }
 }
 
+class ExceptionType extends Exception 
+{
+    public $message;
+
+    public function __construct($message)
+    {
+        parent::__construct($message);
+        $this->message = $message;
+    }
+
+    public function getMyMessage() 
+    {
+        return "<div style='color: #a80b83; 
+        font-weight: 600; 
+        background-color: #ed83d575; 
+        padding: 5px; 
+        margin: 15px 0'>" . $this->message . "</div>";
+    }
+}
+/* 
+Реализовать функцию, вычисляющую факториал числа. Вызов функции произвести в блоке try…catch. Обработку ошибок каждого типа производить в своем классе, наследованным от Exception.  
+*/
+
 function fact($a) {
-    try {
-        if (is_int($a)) {
-            if ($a < 0) {
-                throw new ExceptionMinus('error value is less then 0!');
-            } elseif ($a === 0) {
-                $res = 1;
-            } else {
-                $res = $a * fact ($a - 1);
-            }
+    if (is_int($a)) {
+        if ($a < 0) {
+            throw new ExceptionMinus('error value is less then 0!');
+        } elseif ($a === 0) {
+            $res = 1;
         } else {
-            throw new ExceptionLetter('error value is not numeric!');
+            $res = $a * fact ($a - 1);
         }
-        return $res;
-    } catch (ExceptionLetter | ExceptionMinus $e) {
-        echo $e->getMyMessage() . "Line of exception: " . $e->getLine() . "<br>";
+    } else {
+        throw new ExceptionLetter('error value is not numeric!');
+    }
+    return $res;
+}
+    
+try {
+    // echo fact(5) . "<br>";
+    // echo fact(-5) . "<br>";
+    // echo fact("5") . "<br>";
+    // echo fact(0) . "<br>";
+
+} catch (ExceptionLetter | ExceptionMinus $e) {
+    echo $e->getMyMessage() . "Line of exception: " . $e->getLine() . "<br>";
+}
+
+/* 
+2) Реализовать класс, находящий корни квадратного уравнения. Вызов методов экземпляра класса произвести в блоке try…catch. Обработку ошибок каждого типа производить в своем классе, наследованным от Exception. 
+*/
+
+class Square 
+{
+    public $a;
+    public $b;
+    public $c;
+
+    public function sqrt($mas) {
+        $this->a = $mas[0];
+        $this->b = $mas[1];
+        $this->c = $mas[2];
+    
+            if (count($mas) > 3) {
+                throw new ExceptionType('Слишком много аргументов!');
+            } else if (count($mas) < 3) {
+                throw new ExceptionType('Не хватает аргументов!');
+            }
+
+            if ($this->checkZero()){
+                throw new ExceptionType('Введены все 0!');
+            } else if ($this->check()) {
+                if ($this->a != 0) {
+                    if($this->b == 0 && $this->c == 0) {
+                        $res = "0";
+                    } else if ($this->b == 0 && $this->a / $this->c < 0) {
+                        $x1 = (-$this->c / $this->a) ** 0.5;
+                        $x2 = -((-$this->c / $this->a) ** 0.5);
+                        $res = "$x1; $x2";
+                    } else if ($this->b == 0 && $this->a / $this->c > 0) {
+                        $res = "корней нет";
+                    } else if ($this->c == 0) {
+                        $x1 = 0;
+                        $x2 = -($this->b / $this->a);
+                        $res = "$x1; $x2";
+                    } else if ($this->b != 0 && $this->c != 0) {
+                        $D = $this->b ** 2 - 4 * $this->a * $this->c;
+                        if ($D > 0) {
+                            $x1 = (-$this->b + $D ** 0.5) / (2 * $this->a);
+                            $x2 = (-$this->b - $D ** 0.5) / (2 * $this->a);
+                            $res = "$x1; $x2";
+                        } else if ($D == 0) {
+                            $x = (-$this->b + $D ** 0.5) / (2 * $this->a);
+                            $res = "$x";
+                        } else {
+                            $res = 'корней нет';
+                        }
+                    }
+                } else if ($this->b == 0 && $this->c != 0) {
+                    $res = "корней нет";
+                } else {
+                    // x = -($this->c / $this->b)
+                    // $res = `${x}`
+                    throw new ExceptionType('Введено не квадратное уравнение, а линейное!');
+                }
+                return "Ответ: $res";
+            } else {
+                throw new ExceptionLetter('В параметрах не все числа!');
+            }
+    }
+
+    public function check() {
+        return is_numeric($this->a) && is_numeric($this->b) && is_numeric($this->c);
+    }
+    
+    public function checkZero() {
+        return $this->a == 0 && $this->b == 0 && $this->c == 0;
     }
 }
 
-echo fact("5") . "<br>";
-echo fact(0) . "<br>";
-echo fact(5) . "<br>";
-echo fact(-5) . "<br>";
-
-
-
-
-    function valIsTrue($a) {
-        
-        if (!is_numeric($a)) {
-            $obj = new Exception('error value is not numeric!');
-            throw $obj;
-        }
-        
-        if ($a == 0) {
-            throw new MyException('error y = 0!');
-        }
-
-        if ($a < 5) {
-            throw new MyException2('error value is less 5!');
-        }
-    }
-
-
-    function xDivY($x, $y) {
-        try {
-            valIsTrue($y);
-            return $x / $y;
-        } catch(MyException2 | MyException | Exception $e) {
-            // var_dump($e instanceof Exception); die;
-
-            // var_dump($e instanceof MyException2);
-            // var_dump($e instanceof MyException);
-
-            if ($e instanceof MyException) 
-                echo $e->getMyMessage();
-            else 
-                echo $e->getMessage();
-            
-            throw $e;
-        } /* catch(MyException $e) {
-            // var_dump($e instanceof Exception); die;
-            echo $e->getMyMessage();
-            throw $e;
-        }  */ /* catch(Exception $e) {
-            // var_dump($e instanceof Exception); die;
-            echo $e->getMessage();
-            throw $e;
-        } */
-    }
-
-    // var_dump(xDivY(0, 3));
-    // var_dump(xDivY(2, 0));
-    // var_dump(xDivY(3, 3));
-    // var_dump(xDivY(4, 3));
+$sqrt = new Square();
 
 try {
-    $a = 1 / (xDivY(1, 7) + 3) + xDivY(3, 12);
-    // echo "end - ok";
-} catch(Exception $e) {
-    // var_dump($e->getMessage());
-    // var_dump($e->getLine());
-    // var_dump($e->getFile());
+    echo $sqrt->sqrt([1, 0, -6.9]);
+} catch (ExceptionLetter | ExceptionMinus | ExceptionType $e) {
+    echo $e->getMyMessage() . "Line of exception: " . $e->getLine() . "<br>";
 }
-
-
 
 
